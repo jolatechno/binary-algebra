33245286760085b49f800178e8c4640819c2d3e1
jolatechno <joseph.touzet@gmail.com>
2020-12-26 23:29:06 +0100

    fixed the openmp.hpp file

--- src/openmp.hpp
+++ src/openmp.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
 #if defined(_OPENMP)
+  #define _OPENMP_PRAGMA(all) _Pragma(all)
+
+  #include <omp.h>
+
   #define GPU_LIMIT 10000
   #define CPU_LIMIT 500
+#else
+  #define _OPENMP_PRAGMA(all)
+#endif
 
-  #if defined(_OPENMP)
-    #define _OPENMP_PRAGMA(all) _Pragma(all)
-  #else
-    #define _OPENMP_PRAGMA(all)
-  #endif
-
-  #if defined(_OPENMP) && defined(TARGET)
-    #define _OPENMP_GPU_PRAGMA(gpu) _Pragma(gpu)
-  #else
-    #define _OPENMP_GPU_PRAGMA(gpu)
-  #endif
+#if defined(_OPENMP) && defined(TARGET)
+  #define _OPENMP_GPU_PRAGMA(gpu) _Pragma(gpu)
+#else
+  #define _OPENMP_GPU_PRAGMA(gpu)
 #endif
