d681f65947d47eefe6aef0445cf3940e49f1156c
jolatechno <joseph.touzet@gmail.com>
2021-01-04 09:42:44 +0100

    freed memory in performance testing

--- performance_testing/utils.hpp
+++ performance_testing/utils.hpp
@@ -8,18 +8,22 @@ using namespace std::chrono;
 void timeit(void (*f)(Matrix, Vector), const char* message, int iterations, const int sizes[], int n_sizes) {
   for (int size = 0; size < n_sizes; size++) {
     //initializing matrices
-    Matrix mat(sizes[size], sizes[size]); Vector vect(sizes[size]);
-    mat.randomize(); vect.randomize();
+    Matrix *mat = new Matrix(sizes[size], sizes[size]);
+    Vector *vect = new Vector(sizes[size]);
+    mat->randomize(); vect->randomize();
 
     //reading initial time
     auto start = high_resolution_clock::now();
 
     for (int i = 0; i < iterations; i++)
-      (*f)(mat, vect);
+      (*f)(*mat, *vect);
 
     //reading end time
     auto stop = high_resolution_clock::now();
     double duration = duration_cast<nanoseconds>(stop - start).count()/iterations;
+
+    //delete matrix
+    delete mat;
 
     //printing message
     if (duration < 1000) {
@@ -40,18 +44,23 @@ void timeit(void (*f)(Matrix, Vector), const char* message, int iterations, cons
 void timeit(void (*f)(Matrix, Matrix), const char* message, int iterations, const int sizes[], int n_sizes) {
   for (int size = 0; size < n_sizes; size++) {
     //initializing matrices
-    Matrix mat1(sizes[size], sizes[size]); Matrix mat2(sizes[size], sizes[size]);
-    mat1.randomize(); mat2.randomize();
+    Matrix *mat1 = new Matrix(sizes[size], sizes[size]);
+    Matrix *mat2 = new Matrix(sizes[size], sizes[size]);
+    mat1->randomize(); mat2->randomize();
 
     //reading initial time
     auto start = high_resolution_clock::now();
 
     for (int i = 0; i < iterations; i++)
-      (*f)(mat1, mat2);
+      (*f)(*mat1, *mat2);
 
     //reading end time
     auto stop = high_resolution_clock::now();
     double duration = duration_cast<nanoseconds>(stop - start).count()/iterations;
+
+    //delete matrix
+    delete mat1;
+    delete mat2;
 
     //printing message
     if (duration < 1000) {
@@ -73,18 +82,21 @@ void timeit(void (*f)(Matrix, Matrix), const char* message, int iterations, cons
 void timeit(void (*f)(Matrix), const char* message, int iterations, const int sizes[], int n_sizes) {
   for (int size = 0; size < n_sizes; size++) {
     //initializing matrices
-    Matrix mat(sizes[size], sizes[size]);
-    mat.randomize();
+    Matrix *mat = new Matrix(sizes[size], sizes[size]);
+    mat->randomize();
 
     //reading initial time
     auto start = high_resolution_clock::now();
 
     for (int i = 0; i < iterations; i++)
-      (*f)(mat);
+      (*f)(*mat);
 
     //reading end time
     auto stop = high_resolution_clock::now();
     double duration = duration_cast<nanoseconds>(stop - start).count()/iterations;
+
+    //delete &matrix
+    delete mat;
 
     //printing message
     if (duration < 1000) {
@@ -105,18 +117,23 @@ void timeit(void (*f)(Matrix), const char* message, int iterations, const int si
 void timeit(void (*f)(Vector, Vector), const char* message, int iterations, const int sizes[], int n_sizes) {
   for (int size = 0; size < n_sizes; size++) {
     //initializing matrices
-    Vector vect1(sizes[size]); Vector vect2(sizes[size]);
-    vect1.randomize(); vect2.randomize();
+    Vector *vect1 = new Vector(sizes[size]);
+    Vector *vect2 = new Vector(sizes[size]);
+    vect1->randomize(); vect2->randomize();
 
     //reading initial time
     auto start = high_resolution_clock::now();
 
     for (int i = 0; i < iterations; i++)
-      (*f)(vect1, vect2);
+      (*f)(*vect1, *vect2);
 
     //reading end time
     auto stop = high_resolution_clock::now();
     double duration = duration_cast<nanoseconds>(stop - start).count()/iterations;
+
+    //delete &matrix
+    delete vect1;
+    delete vect2;
 
     //printing message
     if (duration < 1000) {
@@ -137,18 +154,21 @@ void timeit(void (*f)(Vector, Vector), const char* message, int iterations, cons
 void timeit(void (*f)(Vector), const char* message, int iterations, const int sizes[], int n_sizes) {
   for (int size = 0; size < n_sizes; size++) {
     //initializing matrices
-    Vector vect(sizes[size]);
-    vect.randomize();
+    Vector *vect = new Vector(sizes[size]);
+    vect->randomize();
 
     //reading initial time
     auto start = high_resolution_clock::now();
 
     for (int i = 0; i < iterations; i++)
-      (*f)(vect);
+      (*f)(*vect);
 
     //reading end time
     auto stop = high_resolution_clock::now();
     double duration = duration_cast<nanoseconds>(stop - start).count()/iterations;
+
+    //delete &matrix
+    delete vect;
 
     //printing message
     if (duration < 1000) {
